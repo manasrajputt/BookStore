f6b696f147d954731caa9aecfef8914e5985b108
Manas Singh Rajput <manasrajput7470@gmail.com>
2024-05-04 00:14:52 +0530

    footer and navbar.jsx

--- Frontend/BookStore/src/components/Navbar.jsx
+++ Frontend/BookStore/src/components/Navbar.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Navbar() {
 
+    const [ sticky, setSticky ] = useState(false);
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setSticky(true)
+            } else {
+                setSticky(false)
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener(scroll, handleScroll);
+        }
+    }, [])
+
     return (
         <>
-            <div className='max-w-screen-2xl container mx-auto md:px-20 px-4'>
-                <div className="navbar bg-base-100">
+            <div className={`fixed top-0 left-0 right-0 max-w-screen-2xl container mx-auto md:px-20 px-4 ${sticky
+                    ? "sticky-navbar shadow-md bg-base-200 duration-300 transition-all ease-in-out"
+                    : ""
+                }`}>
+                <div className="navbar">
                     <div className="navbar-start">
                         <div className="dropdown">
                             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
